13b406324fd710d42c38db2a8e13e485e148e183
James Iden Busia <iden@ibusia.me>
2025-01-16 22:53:10 -0800

    add nav, add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import React from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
 import Login from './Login'
 import './App.css'
+import Homepage from './Homepage';
 
 function App() {
   return (
-    <>
-    <Login />
-    </>
+    <div className='App'>
+      <nav className='nav'>
+        <Link to="/" className='nav-item'>Homepage</Link>
+        <Link to="/login" className='nav-item'>Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/homepage" element={<Homepage />} />
+      </Routes>
+    </div>
   )
 }
 
