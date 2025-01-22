208f889770a27f3ed17c3106007fc738269fa569
James Iden Busia <iden@ibusia.me>
2025-01-21 22:05:50 -0800

    login button partial, crypto bundling issue

--- src/Login.tsx
+++ src/Login.tsx
@@ -4,13 +4,18 @@ import { useNavigate } from 'react-router-dom';
 
 const { VITE_USERNAME_MAX_LENGTH, VITE_USERNAME_MIN_LENGTH, VITE_PASSWORD_MAX_LENGTH, VITE_PASSWORD_MIN_LENGTH, VITE_COMMUNITY_NAME, VITE_BACKEND_DOMAIN, VITE_BACKEND_PORT, VITE_ENCRYPT_SECRET } = import.meta.env;
 
-export async function login(formData: FormData) {
-    if (!formData.has('username') || !formData.has('password')) {
+export async function login(event: React.FormEvent) {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+        username: { value: string };
+        password: { value: string };
+    }
+    const username = target.username.value;
+    const password = target.password.value;
+    if (!username || !password) {
         console.error("Login failed: username and password required.");
         return null;
     }
-    const username: string = formData.get('username') as string;
-    const password: string = formData.get('password') as string;
 
     const port: string = VITE_BACKEND_PORT ? ':' + VITE_BACKEND_PORT : '';
     const loginUrl: URL = new URL('https://' + VITE_BACKEND_DOMAIN + port + '/login');
@@ -30,7 +35,7 @@ export async function login(formData: FormData) {
 
 function LoginButton() {
     const status = useFormStatus();
-    return <button className="login-register" disabled={status.pending}>Login</button>;
+    return <button className="login-register" type="submit" disabled={status.pending}>Login</button>;
 }
 
 function RegisterButton() {
@@ -44,13 +49,13 @@ function RegisterButton() {
 export default function Login() {
     return (
         <div className='Login'>
-            <form action="login" method='POST'>
+            <form onSubmit={login} method='POST'>
                 <h2>Welcome to {VITE_COMMUNITY_NAME}!</h2>
                 <label htmlFor="username">username</label>
-                <input className="formItem" type="text" name="username" id="username" maxLength={VITE_USERNAME_MAX_LENGTH} minLength={VITE_USERNAME_MIN_LENGTH}></input>
+                <input className="formItem" type="text" name="username" id="username" maxLength={+VITE_USERNAME_MAX_LENGTH} minLength={+VITE_USERNAME_MIN_LENGTH}></input>
                 <br />
                 <label htmlFor="password">password</label>
-                <input className="formItem" type="password" name="password" id="password" maxLength={VITE_PASSWORD_MAX_LENGTH} minLength={VITE_PASSWORD_MIN_LENGTH}></input>
+                <input className="formItem" type="password" name="password" id="password" maxLength={+VITE_PASSWORD_MAX_LENGTH} minLength={+VITE_PASSWORD_MIN_LENGTH}></input>
                 <br />
                 <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
                     <LoginButton />
