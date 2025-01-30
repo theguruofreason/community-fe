17fd844898add70c198cd0138ecd16ff3ef46b9b
James Iden Busia <iden@ibusia.me>
2025-01-29 21:49:50 -0800

    registration page stub

--- src/Registration.tsx
+++ src/Registration.tsx
@@ -1,7 +1,31 @@
 export default function Registration() {
+    async function register(formData: FormData) {
+        'use server'
+        console.log(formData);
+    }
+
     return (
     <div className="Registration">
         <h1>Register!</h1>
+        <form action={register}>
+            <label>
+                <span>username</span>
+                <input name="username" required />
+            </label>
+            <label>
+                <span>password</span>
+                <input name="password" type="password" required />
+            </label>
+            <label>
+                <span>email</span>
+                <input name="email" type="email" />
+            </label>
+            <label>
+                <span>name</span>
+                <input name="name" />
+            </label>
+            <button type="submit">Register</button>
+        </form>
     </div>
     );
 }
