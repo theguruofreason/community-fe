c2d326f342f1d3c8e3292ac3ba9cf32df7d032b6
Iden Busia <iden@ibusia.me>
2025-03-20 21:33:51 -0700

    add default role to env decs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,17 +1,18 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_COMMUNITY_NAME: string,
-    readonly VITE_BACKEND_DOMAIN: string,
-    readonly VITE_BACKEND_PORT: string,
-    readonly VITE_USERNAME_MAX_LENGTH: string,
-    readonly VITE_USERNAME_MIN_LENGTH: string,
-    readonly VITE_PASSWORD_MAX_LENGTH: string,
-    readonly VITE_PASSWORD_MIN_LENGTH: string,
-    readonly VITE_ENCRYPT_SECRET: string,
-    readonly VITE_SALT_ROUNDS: string,
+    readonly VITE_COMMUNITY_NAME: string;
+    readonly VITE_BACKEND_DOMAIN: string;
+    readonly VITE_BACKEND_PORT: string;
+    readonly VITE_USERNAME_MAX_LENGTH: string;
+    readonly VITE_USERNAME_MIN_LENGTH: string;
+    readonly VITE_PASSWORD_MAX_LENGTH: string;
+    readonly VITE_PASSWORD_MIN_LENGTH: string;
+    readonly VITE_ENCRYPT_SECRET: string;
+    readonly VITE_SALT_ROUNDS: string;
+    readonly VITE_DEFAULT_ROLE: string;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv;
 }
