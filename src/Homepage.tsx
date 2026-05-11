619c3cb40ca9fdbd592ff84b75df243ae6c52bae
Iden Busia <theguruofreason@gmail.com>
2026-05-11 12:52:09 -0700

    text posts WIP

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,3 +1,4 @@
+import { useId, useState } from 'react';
 import { useAuth } from './Auth';
 
 export default function Homepage() {
@@ -5,6 +6,28 @@ export default function Homepage() {
     return (
         <div className="Homepage">
             <h1>Welcome to the homepage, {userInfo?.uname}!</h1>
+            <TextPost />
+        </div>
+    );
+}
+
+export function TextPost() {
+    const [postText, setPostText] = useState('');
+    const postTextAreaId = useId();
+    return (
+        <div className="new-text-post">
+            <label htmlFor={postTextAreaId}>
+                New Text Post:
+                <br />
+                <textarea
+                    id={postTextAreaId}
+                    className="post-text-entry"
+                    value={postText}
+                    onChange={(event) => setPostText(event.target.value)}
+                    rows={10}
+                    cols={80}
+                />
+            </label>
         </div>
     );
 }
